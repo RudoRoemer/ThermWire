8e6d29f527d00468da100712dcf3f58667178b90
elena <elena@linux-ma7y.site>
2015-09-15 10:58:39 +0200

    Double checked code, transmision does not work yet

--- src/main.f90
+++ src/main.f90
@@ -1,4 +1,3 @@
-!!!
 PROGRAM ThermWire
 
   IMPLICIT NONE
@@ -61,11 +60,7 @@ PROGRAM ThermWire
      TEMPERATURE(I)=T_L+(I)*(T_R-T_L)/C!-((T_R-T_L)/C)      !QUITE SIMPLE LINEAR PROFILE 
      !write(8,*),TEMPERATURE(I)
   END DO
-!   TEMPERATURE(5)=0.90000000000000013D0
-!   TEMPERATURE(4)=0.70000000000000007D0
-!   TEMPERATURE(3)=0.50000000000000011D0
-!   TEMPERATURE(2)=0.30000000000000004D0
-!   TEMPERATURE(1)=0.10000000000000009D0
+
   !**********************HOPPING************************!
   DO I=1,C
      DO J=1,C
@@ -79,33 +74,26 @@ PROGRAM ThermWire
   END DO
 
   !*********************ENERGY PROFILE******************!
-!!$  DO I=1,N
-!!$     ENERGY(I)=0.0D0 !RANDOM ENERGIES MUST BE INCLUDED
-!!$  END DO
-! ENERGY=0.0D0
+
+ ENERGY=0.0D0
  ENERGY(1)=0.25D0-LAMBDA*LAMBDA/PHENERGY
  ENERGY(2)=0.25D0-LAMBDA*LAMBDA/PHENERGY
   !*********************GAMMA MATRICES*************!
-  ! 
-!!$  DO I=1,Cy
-!!$     DO J=1,C
-!!$        GAMMAL(I,J)=0.0D0
-!!$        GAMMAR(I,J)=0.0D0
-!!$     END DO
-!!$  END DO
+
   GAMMAL=0.0D0
   GAMMAR=0.0D0
 
   ! PLA RESULTS
-  GAMMAL(1,1)=0.2D0 
-  GAMMAL(1,2)=0.2D0*SQRT(b)
-  GAMMAL(2,1)=0.2D0*SQRT(b)
-  GAMMAL(C,C)=0.2D0*b
+  GAMMAL(1,1)=1.0*0.2D0 
+  GAMMAL(1,2)=1.0*0.2D0*SQRT(b)
+  GAMMAL(2,1)=1.0*0.2D0*SQRT(b)
+  GAMMAL(C,C)=1.0*0.2D0*b
 
-  GAMMAR(1,1)=0.2D0*b 
-  GAMMAR(1,2)=0.2D0*SQRT(b)
-  GAMMAR(2,1)=0.2D0*SQRT(b)
-  GAMMAR(C,C)=0.2D0
+  GAMMAR(1,1)=1.0*0.2D0*b 
+  GAMMAR(1,2)=1.0*0.2D0*SQRT(b)
+  GAMMAR(2,1)=1.0*0.2D0*SQRT(b)
+  GAMMAR(C,C)=1.0*0.2D0
+
 
 !   ! LINEAR CHAIN
 !  GAMMAL(1,1)=0.2 
@@ -158,14 +146,7 @@ PROGRAM ThermWire
         ! 
         ! ! !*********************************END CHECKING SOLVER*
 
-!!$        DO I=1,C
-!!$           DO J=1,C
-!!$              GLT(I,J)=0.0D0
-!!$              GGT(I,J)=0.0D0
-!!$              GRR(I,J)=0.0D0
-!!$              GAA(I,J)=0.0D0
-!!$           END DO
-!!$        END DO
+
         GLT=0.0D0
         GGT=0.0D0
         GRR=0.0D0
@@ -173,15 +154,7 @@ PROGRAM ThermWire
        
         !Loop for W-PHENERGY*R to get GGT
         DO R=-M,M
-
-!!$           DO I=1,C
-!!$              DO J=1,C
-!!$                 GR(I,J)=0.0D0
-!!$                 GA(I,J)=0.0D0
-!!$                 GRLT(I,J)=0.0D0
-!!$                 GRGT(I,J)=0.0D0
-!!$              END DO
-!!$           END DO
+        
            GR=0.0D0
            GA=0.0D0
            GRLT=0.0D0
@@ -196,6 +169,7 @@ PROGRAM ThermWire
               DO J=1,C
                  SE(I,J)=-0.5*(0,1)*PHONON_PART(LAMBDA,PHENERGY,TEMPERATURE(I))&
                       *PHONON_PART(LAMBDA,PHENERGY,TEMPERATURE(J))
+        
                  SE(I,J)=SE(I,J)*(GAMMAL(I,J)+GAMMAR(I,J))
                  !write(8,*),W,R,I,J,SE(I,J)
 
@@ -217,38 +191,8 @@ PROGRAM ThermWire
                          SQRT(BOSE(PHENERGY,TEMPERATURE(I))&
                          *(BOSE(PHENERGY,TEMPERATURE(I))+1))
 
-                 !write(8,*),W,R,L_M(I,J)
-!!$                         PHONON_PART(LAMBDA,PHENERGY,TEMPERATURE(I))**2,&
-!!$                         EXP(R*PHENERGY/(TEMPERATURE(I)*2))
-!!$                    write(8,*),W,R,BOSE(PHENERGY,TEMPERATURE(I)),&
-!!$                         SQRT(BOSE(PHENERGY,TEMPERATURE(I))*&
-!!$                         (BOSE(PHENERGY,TEMPERATURE(I))+1))
-<<<<<<< HEAD
-                  !  write(8,*),R,L_M(I,J),BESSEL(R,L_M(I,J)),BESSEL(R,L_M(I,J))&
-                  !       *EXP(R*PHENERGY/(TEMPERATURE(I)*2))
-=======
-                    write(8,*),R,L_M(I,J),BESSEL(R,L_M(I,J)),BESSEL(R,L_M(I,J))&
-                         *EXP(R*PHENERGY/(TEMPERATURE(I)*2))
->>>>>>> 085f9ea939d8590d917148fed04e3d88da595b43
-
-                    !There is some problem here cause for R=1 we should obtain 
-                    !something finite but it seems to be related with 
-                    !higher decimals
                     L_M(I,J)=PHONON_PART(LAMBDA,PHENERGY,TEMPERATURE(I))**2&
                          *EXP(R*PHENERGY/(TEMPERATURE(I)*2))*BESSEL(R,L_M(I,J))
-
-                 !write(8,*),R,L_M(I,J)
-                         
-!                    IF (R==-3) L_M(I,J)=0.0D0
-!                    IF (R==-1) L_M(I,J)=0.0D0
-!                     IF (R==-2) L_M(I,J)=0.0D0
-!                     IF (R==0) L_M(I,J)=0.778801D0
-!                     IF (R==1) L_M(I,J)=0.1947D0
-!                     IF (R==2) L_M(I,J)=0.0243375D0
-!                     IF (R==3) L_M(I,J)=0.00202813D0
-!!$                    write(8,*),W,R,L_M(I,J),&
-!!$                         PHONON_PART(LAMBDA,PHENERGY,TEMPERATURE(I))**2,&
-!!$                         EXP(R*PHENERGY/(TEMPERATURE(I)*2))
 
                  ELSE
                     L_M(I,J)=PHONON_PART(LAMBDA,PHENERGY,TEMPERATURE(I))*&
@@ -261,15 +205,9 @@ PROGRAM ThermWire
               END DO
            END DO
 
-           !DRESSED RETARDED FUNCTION
+           !DRESSED RETARDED FUNCTION GR
            WX=W+ETA-PHENERGY*R
 
-           !FILL THE COEFF. MATRIX 
-!!$           DO Q=1,N
-!!$              DO L=1,N
-!!$                 A(L,Q)=0.0d0
-!!$              END DO
-!!$           END DO
            A=0.0D0
 
            DO IX=1,C 
@@ -298,11 +236,12 @@ PROGRAM ThermWire
                  END DO
               END DO
            END DO
-           DO L=1,N
-                 DO Q=1,N
-                 !write(8,*),L,Q,Real(A(L,Q)),Real((0,1)*A(L,Q))
-                 END DO
-           END DO
+!            DO L=1,N
+!                  DO Q=1,N
+!                  !write(8,*),L,Q,Real(A(L,Q)),Real((0,1)*A(L,Q))
+!                  END DO
+!            END DO
+
            !FILL THE INDEPENDENT TERM 
            DO L=1,N
               TERM(L)=0
@@ -315,10 +254,8 @@ PROGRAM ThermWire
               !write(8,*),Real(TERM(L)),Real((0,1)*TERM(L))
            END DO
 
-!!$           DO L=1,N 
-!!$              X(L)=0.0D0
-!!$           END DO
-           x=0.0D0
+
+           X=0.0D0
 
            !CALL THE LINEAR SYSTEM SOLVER TO GET DRESSED GR
            CALL SOLVER(A,TERM,X) 
@@ -331,22 +268,7 @@ PROGRAM ThermWire
            END DO
            !END IF
 
-           !*****************************************************!
-           !NOW OBTAIN THE RETARDED DRESSED GREEN FUNCTION
-           ! DO I=1,C
-           ! 	DO J=1,C
-           !            IF(I==J) THEN
-           !               GR(I,J)=GR(I,J)
-           !               !write(9,*),I,J,GR(I,J)
-           !            ELSE
-           !               GR(I,J)=AUX(I,J)
-           !               !write(9,*),I,J,GR(I,J)
-           !            END IF
-           !        END DO
-           ! END DO
-           !Only nondiagonal elements with R=0 are relevant
-
-           !DRESSED ADVANCED FUNCTION
+           !DRESSED ADVANCED FUNCTION GA
            DO I=1,C
               DO J=1,C
                  SE(I,J)=-SE(I,J)
@@ -355,12 +277,7 @@ PROGRAM ThermWire
 
            WX=W-ETA-PHENERGY*R
 
-           !FILL THE COEFF. MATRIX 
-!!$           DO Q=1,N
-!!$              DO L=1,N
-!!$                 A(L,Q)=0.0D0
-!!$              END DO
-!!$           END DO
+
            A=0.0D0
            
            DO IX=1,C 
@@ -403,19 +320,10 @@ PROGRAM ThermWire
 
            END DO
 
-!!$           DO L=1,N 
-!!$              X(L)=0.0D0
-!!$           END DO
            X=0.0D0
 
            !CALL THE LINEAR SYSTEM SOLVER TO GET DRESSED GR
            CALL SOLVER(A,TERM,X) 
-           !                                 
-           !THIS SHOULD SOLVE THE LINEAR SYSTEM AND WE GET DRESSED GR IN X
-           !HERE, WE WANT JUST THE DIAGONAL TERMS (THOSE OF GR(1,1)...)
-           !                                
-           !*****THIS SAVES THE DESIRED TERMS IN THE DIAGONAL*****!
-           ! 				
 
            DO L=1,N
               auxrow=INT((L - 1)/C) + 1
@@ -424,34 +332,10 @@ PROGRAM ThermWire
               !write(8,*),L,auxrow,auxcolumn
            END DO
 
-           !*****************************************************!
-           ! 			
-           !NOW OBTAIN THE ADVANCED GREEN FUNCTION BUT ALREADY 
-           !MULTIPLIED BY THE COEFF L_M
-           ! DO I=1,C
-           ! 	DO J=1,C
-           !            IF(I==J) THEN
-           !               GA(I,J)=GA(I,J)
-           !               
-           !            ELSE
-           !               GA(I,J)=AUX(I,J)
-           !               
-           !            END IF
-           !        END DO
-           ! END DO
-           !Only nondiagonal elements with R=0 are relevant
-           !write(8,*),W-PHENERGY*R,R,GA(2,1),GR(2,1)
-           !write(9,*),W-PHENERGY*R,R,SEGT(1,2),SELT(1,2)
-
-           !Here we get GRGT at w-R*w0
+           !Here we get the dressed greater and lesser green function GRGT,GRLT at w-R*w0
            GRGT=MATMUL(GR,MATMUL(SEGT,GA))
            GRLT=MATMUL(GR,MATMUL(SELT,GA))
-!           DO I=1,C
-!               DO J=1,C
-!                 write(81,*),R,I,J,Real(GRGT(I,J)),Real((0,1)*GRGT(I,J))
-!                 write(91,*),R,I,J,Real(GRLT(I,J)),Real((0,1)*GRLT(I,J))
-!                END DO
-!           END DO
+
            DO I=1,C
               DO J=1,C
                  IF(I==J) THEN
@@ -473,13 +357,11 @@ PROGRAM ThermWire
                      
                   IF(I==J) THEN
                      GRR(I,J)=GRR(I,J)+L_M(I,J)*(GR(I,J)+0.5*GRLT(I,J))
-                     !GAA(I,J)=GAA(I,J)+L_M(I,J)*(GR(I,J)+0.5*GRLT(I,J)-GRGT(I,J))
+                    
                   ELSE 
                      IF(R==0) THEN
-                        GRR(I,J)=L_M(I,J)*(GR(I,J)+GRLT(I,J)-GRGT(I,J))
-                        !GAA(I,J)=L_M(I,J)*(GA(I,J)
-                        !write(81,*),W,I,J,Real(GRR(I,J)),Real((0,1)*GRR(I,J))
-                        !write(91,*),W,I,J,Real(GAA(I,J)),Real((0,1)*GAA(I,J))
+                        GRR(I,J)=L_M(I,J)*GR(I,J)
+                        
                      END IF
                   END IF
                   END DO
@@ -489,17 +371,10 @@ PROGRAM ThermWire
         END DO !Loop in R
 
 
-        !Loop for W-PHENERGY*R to get GLT
+        !Loop for W+PHENERGY*R to get GLT
         DO R=-M,M
 
-!!$           DO I=1,C
-!!$              DO J=1,C
-!!$                 GR(I,J)=0.0D0
-!!$                 GA(I,J)=0.0D0
-!!$                 GRLT(I,J)=0.0D0
-!!$                 GRGT(I,J)=0.0D0
-!!$              END DO
-!!$           END DO
+
            GR=0.0D0
            GA=0.0D0
            GRLT=0.0D0
@@ -534,18 +409,7 @@ PROGRAM ThermWire
                          SQRT(BOSE(PHENERGY,TEMPERATURE(I))&
                          *(BOSE(PHENERGY,TEMPERATURE(I))+1))
 
-                 !write(8,*),W,R,L_M(I,J)
-!!$                         PHONON_PART(LAMBDA,PHENERGY,TEMPERATURE(I))**2,&
-!!$                         EXP(R*PHENERGY/(TEMPERATURE(I)*2))
-!!$                    write(8,*),W,R,BOSE(PHENERGY,TEMPERATURE(I)),&
-!!$                         SQRT(BOSE(PHENERGY,TEMPERATURE(I))*&
-!!$                         (BOSE(PHENERGY,TEMPERATURE(I))+1))
-                  !  write(8,*),R,L_M(I,J),BESSEL(R,L_M(I,J)),BESSEL(R,L_M(I,J))&
-                  !       *EXP(R*PHENERGY/(TEMPERATURE(I)*2))
 
-                    !There is some problem here cause for R=1 we should obtain 
-                    !something finite but it seems to be related with 
-                    !higher decimals
                     L_M(I,J)=PHONON_PART(LAMBDA,PHENERGY,TEMPERATURE(I))**2&
                          *EXP(R*PHENERGY/(TEMPERATURE(I)*2))*BESSEL(R,L_M(I,J))
                     
@@ -565,12 +429,7 @@ PROGRAM ThermWire
            !DRESSED RETARDED FUNCTION
            WX=W+ETA+PHENERGY*R
 
-           !FILL THE COEFF. MATRIX 
-!!$           DO Q=1,N
-!!$              DO L=1,N
-!!$                 A(L,Q)=0.0D0
-!!$              END DO
-!!$           END DO
+
            A=0.0D0
 
            DO IX=1,C 
@@ -612,41 +471,20 @@ PROGRAM ThermWire
               END IF
            END DO
 
-!!$           DO L=1,N 
-!!$              X(L)=0.0D0
-!!$           END DO
+
            X=0.0D0
 
            !CALL THE LINEAR SYSTEM SOLVER TO GET DRESSED GR
            CALL SOLVER(A,TERM,X) 
 
-           !THIS SHOULD SOLVE THE LINEAR SYSTEM AND WE GET DRESSED GR IN X
-           !HERE, WE WANT JUST THE DIAGONAL TERMS (THOSE OF GR(1,1)...)
-
-           !*****THIS SAVES THE DESIRED TERMS IN THE DIAGONAL*****!
+         
            DO L=1,N
               auxrow=INT((L - 1)/C) + 1
               auxcolumn=MOD((L - 1),C) + 1
               GR(auxrow,auxcolumn)=X(L) 
               !write(8,*),L,auxrow,auxcolumn
            END DO
-           !END IF
-           !*****************************************************!
-
-           !NOW OBTAIN THE RETARDED DRESSED GREEN FUNCTION
-           ! DO I=1,C
-           ! 	DO J=1,C
-           !            IF(I==J) THEN
-           !               GR(I,J)=GR(I,J)
-           !               !write(9,*),I,J,GR(I,J)
-           !            ELSE
-           !               GR(I,J)=AUX(I,J)
-           !               !write(9,*),I,J,GR(I,J)
-           !            END IF
-           !        END DO
-           ! END DO
-           !write(8,*),W+PHENERGY*R,GR(1,1),GR(2,2)
-
+          
            !DRESSED ADVANCED FUNCTION
            DO I=1,C
               DO J=1,C
@@ -657,12 +495,6 @@ PROGRAM ThermWire
 
            WX=W-ETA+PHENERGY*R
 
-           !FILL THE COEFF. MATRIX 
-!!$           DO Q=1,N
-!!$              DO L=1,N
-!!$                 A(L,Q)=0.0D0
-!!$              END DO
-!!$           END DO
            A=0.0D0
 
            DO IX=1,C 
@@ -707,18 +539,11 @@ PROGRAM ThermWire
 
            END DO
 
-!!$           DO L=1,N 
-!!$              X(L)=0.0D0
-!!$           END DO
            X=0.0D0
 
-           !CALL THE LINEAR SYSTEM SOLVER TO GET DRESSED GR
+           !CALL THE LINEAR SYSTEM SOLVER TO GET DRESSED GA
            CALL SOLVER(A,TERM,X) 
 
-           !THIS SHOULD SOLVE THE LINEAR SYSTEM AND WE GET DRESSED GR IN X
-           !HERE, WE WANT JUST THE DIAGONAL TERMS (THOSE OF GR(1,1)...)
-
-           !*****THIS SAVES THE DESIRED TERMS IN THE DIAGONAL*****!
 
            DO L=1,N
               auxrow=INT((L - 1)/C) + 1
@@ -749,32 +574,39 @@ PROGRAM ThermWire
            DO I=1,C
               DO J=1,C
                    IF(I==J) THEN
-                    GRR(I,J)=GRR(I,J)-L_M(I,J)*(0.5*GRGT(I,J))
-                    !GAA(I,J)=GAA(I,J)+L_M(I,J)*(0.5*GRLT(I,J))
-                   !ELSE 
-                   !IF(R==0) THEN
-                      !GRR(I,J)=L_M(I,J)*GR(I,J)
-                      !GAA(I,J)=L_M(I,J)*GA(I,J)
-                      !write(81,*),W,I,J,Real(GRR(I,J)),Real((0,1)*GRR(I,J))
-                      !write(91,*),W,I,J,Real(GAA(I,J)),Real((0,1)*GAA(I,J))
-                   !END IF
-                 END IF
+                    GRR(I,J)=GRR(I,J)-L_M(I,J)*(0.5*GRLT(I,J))
+                    
+                   ELSE 
+                  
+                   END IF
               END DO
            END DO
            !write(81,*),W,I,J,Real(GRR(1,1)),Real((0,1)*GRR(1,1))
            !write(91,*),W,I,J,Real(GAA(1,1)),Real((0,1)*GAA(1,1))
 
         END DO !Loop in R
+              
+    
 
-        GAA=GRR+GLT-GGT          
-        !SPECTRAL=(0,1)*(GGT-GLT)
+        GAA=GRR+GLT-GGT 
+!               write(8,*),W,Real(GRR(1,1)),Real((0,1)*GRR(1,1)),Real(GRR(2,2)),Real((0,1)*GRR(2,2))
+!               write(9,*),W,Real(GAA(1,1)),Real((0,1)*GAA(1,1)),Real(GAA(2,2)),Real((0,1)*GAA(2,2))
+!               write(81,*),W,Real(GRR(1,2)),Real((0,1)*GRR(1,2)),Real(GRR(2,1)),Real((0,1)*GRR(2,1))
+!               write(91,*),W,Real(GAA(1,2)),Real((0,1)*GAA(1,2)),Real(GAA(2,1)),Real((0,1)*GAA(2,1))
+!                   
+!               write(82,*),W,Real(GLT(1,1)),Real((0,1)*GLT(1,1)),Real(GLT(2,2)),Real((0,1)*GLT(2,2))
+!               write(92,*),W,Real(GGT(1,1)),Real((0,1)*GGT(1,1)),Real(GGT(2,2)),Real((0,1)*GGT(2,2))
+!               write(83,*),W,Real(GLT(1,2)),Real((0,1)*GLT(1,2)),Real(GLT(2,1)),Real((0,1)*GLT(2,1))
+!               write(93,*),W,Real(GGT(1,2)),Real((0,1)*GGT(1,2)),Real(GGT(2,1)),Real((0,1)*GGT(2,1))
+                    
+        SPECTRAL=(0,1)*(GGT-GLT)
         !SPECTRAL=(0,1)*(GRR-GAA)
-        TRANSCOEFF=MATMUL(MATMUL(GLT,GAMMAR),MATMUL(GGT,GAMMAL))
-        SPECTRAL=MATMUL(GGT,GAMMAL)
-        !TRANSCOEFF=GAA
+        TRANSCOEFF=MATMUL(MATMUL(GAA,GAMMAR),MATMUL(GRR,GAMMAL))
+        !TRANSCOEFF=MATMUL(GAMMAR,MATMUL(GRR,GAMMAL))
+        !SPECTRAL=MATMUL(GGT,GAMMAL)
+        !TRANSCOEFF=(0,1)*(GRR-GAA)
         
-        write(81,*),W,GLT(1,1),GLT(1,1)
-        write(91,*),W,GAA(1,1),GAA(1,1)
+    
      
         
         AUX=MATMUL(GAMMAL-GAMMAR,(0,1)*GLT)+MATMUL(GAMMAL*FERMI(W,MU_L,T_L)-GAMMAR*FERMI(W,MU_R,T_R),SPECTRAL)        
@@ -920,25 +752,7 @@ CONTAINS
     END SELECT
 
   END FUNCTION BESSEL
-
-  ! DOUBLE PRECISION FUNCTION BESSEL(N,X)
-  ! 	IMPLICIT NONE
-  ! 	INTEGER::N,N1
-  ! 	DOUBLE PRECISION::X
-  ! 	DOUBLE PRECISION::OUTPUT,BK
-  ! 	INTEGER::K,P,M
-  ! 		N1=ABS(N)
-  ! 		M=6
-  ! 		OUTPUT=0
-  ! 		P=1
-  ! 		IF(N<0) P=(-1)**N
-  ! 		DO K=0,M
-  ! 			BK=1./((2**(2*K+N1))*FACTORIAL(K)*FACTORIAL(K+N1))
-  ! 			OUTPUT=OUTPUT+BK*((X)**(2*K+N1))
-  ! 		END DO
-  ! 		OUTPUT=OUTPUT*P
-  ! 	BESSEL=ABS(OUTPUT)
-  ! END FUNCTION BESSEL
+  
 
   SUBROUTINE SOLVER(MAT, RHS, VEC)
     COMPLEX*16::MAT(N,N), RHS(N), VEC(N)
